344878178835c9896ee8032463c72a218fbb15c9
bonb1 <40760684+bonb1@users.noreply.github.com>
2020-06-03 14:41:57 +0200

    Corrected Files 3.6.2020

--- JSmulticonverter.js
+++ JSmulticonverter.js
@@ -1,116 +1,78 @@
 
 
 // Starte hier:
-//Drei Conversion Formeln definieren: 
+//Drei Conversion Funktionen definieren:
 //knots/kilometer, fahrenheit/celsius/, inch/centimeter
 
-function nodeSpeed(hoursKilometer) {
-    return (hoursKilometer * 1.852);
+function knotsTokmh(knots) {
+    return knots * 1.852;
+}
+
+function fahrenheitTocelsius(fahrenheit) {
+    return (fahrenheit - 32) * 5/9;
+}
+
+function inchesTocentimeter(inches) {
+    return inches * 2.54;
 }
 
 
-function fahrenheit(celsius) {
-    return((celsius - 32) * 5/9);
-}
 
-function inch(centimeter) {
-    return (centimeter * 2.54);
-}
-
-
-
-//Ask user if user wants to convert Y/N
+//function: Ask if user wants to convert one of the elements Y/N
 
 function askContinue() {
     var answer = window.prompt("Do you want to convert a value? Print: Yes or No?");
-    
-    if(answer == "Yes") {
-        return true;        //Why Boolean and not if, else if and else?
-    }
-    else if(answer == "yes") {
-        return true;
-    } 
-    else {
-        return false;
-    }
-}   
 
+    if(answer == "yes") {
+        return true;        //Why Boolean and not if, else if and else?
+    } else {
+        return window.prompt("Good by!");
+        
+    }
     
+    //else if(answer == "yes") {
+        //return true;
+    
+    //else {
+        //return window.prompt("Good bye!");
+    //}
+}
+
+
 
 
 //Ask user to select conversion type
-function askSelect() {    
-    var answer = window.prompt("Which conversion would you like to do? knots / fahrenheit / inches");
-        return answer;
+function askSelect() {
+    var answer = window.prompt("Which conversion would you like to do? knots / fahrenheit  / inches");
+    return answer;
 }
-   
 
-    
-//Ask user to enter a value
-function askValue() {
-    var answer = window.prompt("Please enter a value to convert");
-        return answer;
+//Ask user for a value:
+
+function askValue(){
+    var answer = window.prompt("Please enter a number");
+    return answer;
 }
-    
 
-
-
-
-
-
-//Check which conversion: (hier weiter BAUSTELLE)
-
-
+// while-loop, boolean value:
 while(askContinue()) {
     var conversionType = askSelect();
     var conversionValue = askValue();
-    var resultMessage = "The result of the conversion is ";
+    var resultMessage = "The result of the conversion is ";     // ev if, else if, else, einbauen
+    
 
+    if(conversionType == "knots"){
+        resultMessage += knotsTokmh(Number(conversionValue)).toString() + " Kmh";
+    } else if (conversionType == "fahrenheit") {
+        resultMessage += fahrenheitTocelsius(Number(conversionValue)).toString() + " Degrees Celsius";
+    } else if(conversionType == "inches") {
+        resultMessage += inchesTocentimeter(Number(conversionValue)).toString() + " Centimeter";
+    } else {
+        resultMessage = "This conversion is not possible. Please check your input!";
+    }
+    console.log(resultMessage); //result in the console
+    window.prompt(resultMessage);    // result in the window   
 
-    if (conversionType == "knots") {
-        resultMessage += nodeSpeed(Number(conversionValue)).toString();  //1. Umwandlung in Zahl (Number) 2. Das ganze zurück in Text = .toString
-          
-    }
-    else if (conversionType == "fahrenheit") {
-        resultMessage += fahrenheit(Number(conversionValue)).toString(); 
-    }
-    else if (conversionType == "inches") {
-        resultMessage += inch(Number(conversionValue)).toString(); 
-    }   else {
-        resultMessage = "Sorry. I can't do this conversion";
-
-    }
 }
 
-console.log(resultMessage);
 
-
-
-
-
-
-
-
-// Main:
-// -Continue while user wants to convert something.
-
-
-
-//-Check which conversion
-
-//- Get value
-
-// -display result
-
-
-
-    
-    
-
-
-//try: except: quit() bei fehlerhafter Eingabe durch den user
-
-// vom dritten Fenster wieder auf das erste springen (yes) oder quit bei (no):
-//Do you want to convert a value: yes or no? 
-
-//nicht vergessen: Einzahl und Mehrzahl programmieren z.b. knot und knots
